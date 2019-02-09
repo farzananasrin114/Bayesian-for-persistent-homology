4e6f80889d1de54b91dd0c074ed1ffc2b16149f0
farzananasrin114 <47154022+farzananasrin114@users.noreply.github.com>
2019-02-09 13:19:08 -0500

    Update Wedge_Gaussian.R

--- BayesTDA/R/Wedge_Gaussian.R
+++ BayesTDA/R/Wedge_Gaussian.R
@@ -4,10 +4,10 @@
 #' @usage Wedge_Gaussian(z, mean, sigma)
 #' @param z: two element vector. These are the points to evaluate density at.
 #' @param mean: two element vector, mean of the restricted Gaussian
-#' @param sigma: positive constant. Covariance matrix of the restricted Gaussian has the form sigma*I_2, where I_2 is the 2x2 indentity matrix.
+#' @param sigma: positive constant. Covariance matrix of the restricted Gaussian has the form sigma*I_2, where I_2 is the 2x2 identity matrix.
 #' @details The function \code{Wedge_Gaussian} computes Gaussian density restricted to wedge W. 
 #' For a death vs. birth persistence diagram wedge is defined as the two dimensional coordinate (b,d) such that d>b>0. 
-#' For a tilted represetation, i.e., persistence vs. birth persistence diagram wedge is defined as T(b,d) = (b, d-b).
+#' For a tilted representation, i.e., persistence vs. birth persistence diagram wedge is defined as T(b,d) = (b, d-b).
 #' @return The function \code{Wedge_Gaussian} returns numerical value of the  Gaussian density restricted to wedge W.
 #' @description The function \code{Wedge_Gaussian} computes Gaussian density restricted to wedge W. Require \code{mvtnorm} package for computing multivariate normal pdf and cdf.
 #' @examples # input the mean and constant sigma of covariance matrix
@@ -24,26 +24,3 @@ Wedge_Gaussian = function(z,mean,sigma){
   num = dmvnorm(z,mean = mean, sigma = sigma*diag(2))
   return(num)
 }
-
-
-# Wedge_Gaussian <- setRefClass('Wedge_Gaussian',
-# 
-#                               fields=list(mean='numeric',sigma='numeric'),
-#                               # mean: two element vector, mean of the modified Gaussian from Oballe, Maroulas 2018
-#                               # sigma: positive constant, sigma in covariance matrix from Oballe, Maroulas 2018
-# 
-# 
-#                               methods = list(
-#                                 initialize = function(mean,sigma){
-#                                   .self$mean = mean
-#                                   .self$sigma = sigma
-#                                 },
-# 
-#                                 evaluate = function(x){
-#                                   # x: two element numeric
-#                                   # return value of wedge gaussian density at x
-# 
-#                                   return(dmvnorm(x,mean=.self$mean,sigma=.self$sigma*diag(2)))
-#                                 }
-#                               )
-# )
