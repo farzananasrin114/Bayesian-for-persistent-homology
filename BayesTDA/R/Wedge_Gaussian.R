fb4ed7626c586ddf895a4c673e5619a008381e29
farzananasrin114 <47154022+farzananasrin114@users.noreply.github.com>
2019-02-11 11:20:33 -0500

    Update Wedge_Gaussian.R

--- BayesTDA/R/Wedge_Gaussian.R
+++ BayesTDA/R/Wedge_Gaussian.R
@@ -1,5 +1,5 @@
 #' Restricted Gaussian density
-#' The function \code{Wedge_Gaussian} computes Gaussian density restricted to wedge W.
+#' @description The function \code{Wedge_Gaussian} computes Gaussian density restricted to wedge W. Require \code{mvtnorm} package for computing multivariate normal pdf and cdf.
 #' @name Wedge_Gaussian
 #' @usage Wedge_Gaussian(z, mean, sigma)
 #' @param z: two element vector. These are the points to evaluate density at.
@@ -9,7 +9,6 @@
 #' For a death vs. birth persistence diagram wedge is defined as the two dimensional coordinate (b,d) such that d>b>0. 
 #' For a tilted representation, i.e., persistence vs. birth persistence diagram wedge is defined as T(b,d) = (b, d-b).
 #' @return The function \code{Wedge_Gaussian} returns numerical value of the  Gaussian density restricted to wedge W.
-#' @description The function \code{Wedge_Gaussian} computes Gaussian density restricted to wedge W. Require \code{mvtnorm} package for computing multivariate normal pdf and cdf.
 #' @examples # input the mean and constant sigma of covariance matrix
 #' w <- Wedge_Gaussian(z = c(1,2), mean = c(1,1), sigma = 0.1)
 
